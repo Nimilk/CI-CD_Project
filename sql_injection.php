7d7cdc0903fe6e0159fdb33ab164f3379aeaaadf
Nimilk <nk23acz@herts.ac.uk>
2025-05-01 11:20:55 +0100

    Update sql_injection.php

--- sql_injection.php
+++ sql_injection.php
@@ -2,7 +2,7 @@
 $id = $_GET["id"];
 $password = "123";
 $con = mysqli_connect("localhost", "root", $password, "test");
-  $sql = "SELECT id, fname, lname FROM persons WHERE id=$id";
+  $sql = "SELECT id, fname, lname FROM persons WHERE id=id";
 $result = mysqli_query($con,$sql);
 if ($result->num_rows > 0) {
 while($row = $result->fetch_assoc()) {
